07d058e8e3252cd70fd5fa862209866ff65c960c
nagajyothijakka26 <jnjyothi26@gmail.com>
2024-02-14 20:41:09 +0530

    routing changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,2 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
-export const B=()=>{};
-
-export function C() {
-  return <h1>Hellow...Dhoni</h1>
-
-}
-
-const A=()=>{
-  return <h1>A</h1>;
-
-};
-export default A;
+import Home from "./home/page";
+export default Home;
