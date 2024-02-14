326487d78dc05eb11341472e0f85dfc6fd637fdd
nagajyothijakka26 <jnjyothi26@gmail.com>
2024-02-14 17:30:31 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export const B=()=>{};
+
+export function C() {
+  return <h1>Hellow...Dhoni</h1>
+
+}
+
+const A=()=>{
+  return <h1>A</h1>;
+
+};
+export default A;
